6e0d5bee638bc7228701260f6589ab0a0355166f
x_star_cat <32558367+andylogan@users.noreply.github.com>
2020-01-12 11:14:57 -0600

    Add files via upload

---
+++ PalindromeDetector Complete/PalindromeDetector.Tests/PalindromeDetector_Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeDetector.Tests
+{
+    [TestFixture]
+    public class PalindromeDetector_Tests
+    {
+        [Test]
+        public void Correctly_identifies_palindromes()
+        {
+            var subject = new PalindromeDetector();
+
+            Assert.IsTrue(subject.IsPalindrome("noon"));
+            Assert.IsTrue(subject.IsPalindrome("redivider"));
+            Assert.IsTrue(subject.IsPalindrome("Apollo, PA"));
+            Assert.IsTrue(subject.IsPalindrome("12345678987654321"));
+            Assert.IsTrue(subject.IsPalindrome("Was it a car or a cat I saw?"));
+            Assert.IsTrue(subject.IsPalindrome("Step on no pets."));
+            Assert.IsTrue(subject.IsPalindrome("racecar"));
+            Assert.IsTrue(subject.IsPalindrome("Hannah"));
+            Assert.IsTrue(subject.IsPalindrome("deified"));
+        }
+
+        [Test]
+        public void Correctly_identifies_non_palindromes()
+        {
+            var subject = new PalindromeDetector();
+
+            Assert.IsFalse(subject.IsPalindrome("asdf"));
+            Assert.IsFalse(subject.IsPalindrome("lorem ipsum"));
+            Assert.IsFalse(subject.IsPalindrome("Service Management Group"));
+        }
+    }
+}
---
+++ PalindromeDetector Complete/PalindromeDetector/Palindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PalindromeDetector
+{
+    public class PalindromeDetector
+    {
+        public bool IsPalindrome(string candidate)
+        {
+            var result = GroomInput(candidate);
+            var x = 0;
+            var y = result.Length - 1;
+
+            do
+            {
+                if (x >= y) return true;
+            }
+            while (result[x++] == result[y--]);
+
+            return false;
+        }
+
+        public string GroomInput(string input)
+        {
+            var output = "";
+
+            foreach(var character in input)
+            {
+                if (char.IsLetterOrDigit(character))
+                {
+                    output += character;
+                }
+            }
+            return output.ToLower();
+        }
+    }
+}
